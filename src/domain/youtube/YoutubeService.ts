1cf929f30308f1edceb764d6b9e4f2e9f52142f7
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-06 13:37:21 +0300

    Calling getting video info from service

---
+++ src/domain/youtube/YoutubeService.ts
@@ -0,0 +1,13 @@
+import { YoutubeRepository } from "./YoutubeRepository";
+import { getYoutubeId } from "./youtubeUtils";
+import { parseVideoInfoSchema } from "./youtubeValidation";
+
+class YoutubeService {
+  constructor(private youtubeRepository: YoutubeRepository) {}
+
+  public async getVideoInfo(data: any) {
+    const { url } = parseVideoInfoSchema(data);
+    const id = getYoutubeId(url);
+    await this.youtubeRepository.getVideoInfo(id);
+  }
+}
