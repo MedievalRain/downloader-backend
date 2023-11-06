110fa5b69d3fd26b90111c5b5d284c215766c95b
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-06 14:16:54 +0300

    Check error responses

--- src/index.test.ts
+++ src/index.test.ts
@@ -11,6 +11,11 @@ describe("/api/youtube/info", () => {
     const response = await request(app).get(baseUrl);
 
     expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: "Bad input",
+      }),
+    );
   });
 
   it("should respond with 400 status when not youtube url", async () => {
@@ -18,11 +23,21 @@ describe("/api/youtube/info", () => {
     const response = await request(app).get(baseUrl).query({ url });
 
     expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: "Bad input",
+      }),
+    );
   });
   it("should respond with 400 status when no id in url found", async () => {
     const url = "https://youtube.com?b=xcq";
     const response = await request(app).get(baseUrl).query({ url });
     expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: "Video ID not found",
+      }),
+    );
   });
   it("should respond with 200 status when found id", async () => {
     const url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
