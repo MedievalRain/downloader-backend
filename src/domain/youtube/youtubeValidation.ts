2b1aeca6a8ed65882c5393f62b77f2feaa2ea8e5
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-06 14:24:55 +0300

    Create custom validation for youtube url

--- src/domain/youtube/youtubeValidation.ts
+++ src/domain/youtube/youtubeValidation.ts
@@ -1,8 +1,19 @@
 import z from "zod";
 import { ValidationError } from "../../types/errors";
 
+const isYoutubeUrl = (url: string) => {
+  try {
+    const parsedUrl = new URL(url);
+    const isHttps = parsedUrl.protocol === "https:";
+    const isYoutubeDomain = ["youtube.com", "youtu.be"].includes(parsedUrl.hostname.replace("www.", ""));
+    return isHttps && isYoutubeDomain;
+  } catch (error) {
+    return false;
+  }
+};
+
 const getVideoInfoSchema = z.object({
-  url: z.string().min(8).max(256).startsWith("https://youtu"),
+  url: z.string().refine(isYoutubeUrl),
 });
 
 export const parseVideoInfoSchema = (data: any) => {
