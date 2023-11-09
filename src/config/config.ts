0b3337fdc85e80b5623bb52ae2c8fb645369be98
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-09 23:44:14 +0300

    Delete unused env valriable

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 export const PORT = process.env.PORT!;
-export const CORS_URL = process.env.CORS_URL!;
 
-if (!(PORT && CORS_URL)) {
+if (!PORT) {
   throw new Error("Missing environment variables. Check your .env file.");
 }
